d0862932065604b1415ad7c15533e584bca5b08c
Thabo Moagi <tboydaflame@gmail.com>
2026-03-29 23:22:09 +0200

    Add premium dashboard UI

--- stokpay/urls.py
+++ stokpay/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from contributions.views import MemberViewSet, ContributionViewSet, StokvelViewSet, PayoutViewSet
@@ -11,6 +12,7 @@ router.register(r'contributions', ContributionViewSet)
 router.register(r'payouts', PayoutViewSet)
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='dashboard.html'), name='dashboard'),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
